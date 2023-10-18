a0f789d85fb831a2a84ba24889bd6e8aec57ae4e
antony oduor <antonyoduor72@gmail.com>
2023-10-18 18:12:18 +0300

    All

--- C1.java/src/Car.java
+++ C1.java/src/Car.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Car {
+import java.util.Scanner;
+
+public class Car {
+    private String name;
+    private int age;
+    private int dob;
+    int j;
+Scanner sc = new Scanner(System.in);
+void scanner()
+
+    {
+
+            System.out.print("Enter your name: ");
+            name = sc.nextLine();
+
+            System.out.print("How old are you: ");
+            age = sc.nextInt();
+
+            System.out.print("What year were you born: ");
+            dob = sc.nextInt();
+
+    }
+     void getStuff(String day, String date){
+
+        System.out.println("My name is " + name + " i am " + age + " years old. I was born in " + dob);
+        System.out.println("Today is " + day + ". Date:  " + date );
+    }
+
+    public Car(String name, int age, int dob) {
+        this.name = name;
+        this.age = age;
+        this.dob = dob;
+    }
 }
---
+++ C1.java/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+         Car cr = new Car("", 0, 0);
+         cr.scanner();
+         cr.getStuff("Wednesday", "27-09-2023");
+
+    }
+}
